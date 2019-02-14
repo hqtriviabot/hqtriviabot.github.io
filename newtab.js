3d4eece4d26a07674aa71b997d713013586057c9
hqtriviabot <37088847+hqtriviabot@users.noreply.github.com>
2019-02-13 19:15:28 -0500

    Update newtab.js

--- newtab.js
+++ newtab.js
@@ -1,6 +1,10 @@
 var num1 = prompt("whats 1 plus 1" , "put it here (its 2)")
 if(num1 = 2){
     while(1){
+        tab()
+        setTimeout(myFunction, 1000)
+        tab(){
         window.open("chrome://newtab")
+        }
     }
     }
