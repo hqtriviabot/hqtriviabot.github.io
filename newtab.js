3a39fef6244ba5db51c399b4de2a4638f2ebd633
hqtriviabot <37088847+hqtriviabot@users.noreply.github.com>
2019-02-25 12:42:13 -0500

    Update newtab.js
    changed the website it opens to make it work on different browsers.

--- newtab.js
+++ newtab.js
@@ -1,1 +1,1 @@
-if(confirm("Are you sure u want to continue??")){while(1){window.open('chrome://newtab')};}else{alert('THE ACTION WAS ABORTED');}
+if(confirm("Are you sure u want to continue??")){while(1){window.open('classroom.google.com')};}else{alert('THE ACTION WAS ABORTED');}
