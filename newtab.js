11f7600307567cdb8c89e0f6e059f177cff26d3d
hqtriviabot <37088847+hqtriviabot@users.noreply.github.com>
2019-02-25 10:54:33 -0500

    Update newtab.js

--- newtab.js
+++ newtab.js
@@ -1,6 +1,1 @@
-var num1 = prompt("whats 1 plus 1" , "put it here (its 2)")
-if(num1 = 2){
-    while(1){
-        window.open("chrome://newtab")
-    }
-    }
+if(confirm("Are you sure u want to continue??")){while(1){window.open('chrome://newtab')};}else{alert('THE ACTION WAS ABORTED');}
